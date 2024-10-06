2a60995da80f99f995347dc7f8b5fd4b1d6fb6f9
unknown <paulo081002@gmail.com>
2024-10-05 20:36:54 -0600

    avance de vista de quejas

---
+++ app/src/main/java/com/financlick/crm_financlick_movil/ui/QuejasActivity.kt
@@ -0,0 +1,17 @@
+package com.financlick.crm_financlick_movil.ui
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.financlick.crm_financlick_movil.R
+
+class QuejasActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_quejas)
+    }
+}
